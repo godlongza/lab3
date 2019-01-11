ab27363614dae70a7b187d0faf9a18707569d0a7
lab303-5 <lab303-5@labeng.local>
2019-01-11 17:10:04 +0700

    ex 3

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main(){
 
-cout << "Hello";
-
-return 0;
+    float x;
+    float sum;
+    float y = 2;
+    while (y <=69){
+        x = 1/y;
+        y++;
+        sum=sum + x;
+    }
+    cout << sum;
+    return 0;
 }
